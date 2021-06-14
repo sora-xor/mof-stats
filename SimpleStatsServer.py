365d66bc92d0ee05a9baaeb368ef8cad52f8a95f
武宮誠 <takemiya@soramitsu.co.jp>
2021-06-14 12:23:59 +0400

    add qty api

--- SimpleStatsServer.py
+++ SimpleStatsServer.py
@@ -1,13 +1,30 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import requests
+import json
+
+server = "https://rpc.sora2.soramitsu.co.jp:/9933"
+
+headers = {
+    'Content-Type': 'application/json',
+}
 
 class getToken(tornado.web.RequestHandler):
     def get(self):
         self.write("hello")
 
+class QtyHandler(tornado.web.RequestHandler):
+    def get(self, symbol):
+        data = '{"id":1, "jsonrpc":"2.0", "method": "assets_totalSupply", "params":["0x0200000000000000000000000000000000000000000000000000000000000000"]}'
+        response = requests.post(server, headers=headers, data=data)
+        balance = json.loads(response.content)['result']['balance']
+        balance = balance[:-18] + '.' + balance[-18:]
+        self.write(balance)
+
 application = tornado.web.Application([
     (r'/', getToken),
+    (r"/qty/([\d\w]+)", QtyHandler)
 ])
 
 if __name__ == '__main__':
